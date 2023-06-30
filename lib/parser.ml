d2d32b1772926c25ff6440c78283c8745f1b6151
Gustaf Holm <gustaf.holm@icloud.com>
2023-06-30 07:29:03 +0200

    refactor: break out precedence to own module

--- lib/parser.ml
+++ lib/parser.ml
@@ -7,43 +7,6 @@ type t =
   }
 [@@deriving show]
 
-module Precedence = struct
-  type t =
-    | Lowest
-    | Equals
-    | LessGreater
-    | Sum
-    | Product
-    | Prefix
-    | Call
-  [@@deriving show]
-
-  let to_int = function
-    | Lowest -> 1
-    | Equals -> 2
-    | LessGreater -> 3
-    | Sum -> 4
-    | Product -> 5
-    | Prefix -> 6
-    | Call -> 7
-  ;;
-
-  let of_token = function
-    | Token.Eq -> Equals
-    | Token.NotEq -> Equals
-    | Token.Lt -> LessGreater
-    | Token.Gt -> LessGreater
-    | Token.Plus -> Sum
-    | Token.Minus -> Sum
-    | Token.Slash -> Product
-    | Token.Asterisk -> Product
-    | _ -> Lowest
-  ;;
-
-  let lt t1 t2 = to_int t2 < to_int t1
-  let gt t1 t2 = to_int t2 > to_int t1
-end
-
 type statement_result = (Ast.statement, string) Result.t
 type expression_result = (Ast.expression, string) Result.t
 type infix_parser_fn = t -> expression_result * t
